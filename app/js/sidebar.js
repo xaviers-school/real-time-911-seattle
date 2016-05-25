a1c570c090761766660270b93b42c6a10af59dc0
SABRINA TEE <sabrinatee@me.com>
2016-05-24 17:56:37 -0700

    Sidebar populating from most recent

--- app/js/sidebar.js
+++ app/js/sidebar.js
@@ -2,6 +2,12 @@ function displayList(markerData) {
   var theTemplate;
   var content = {};
 
+  markerData.sort(function (a, b) {
+    if (a.datetime < b.datetime) return 1;
+    if (a.datetime > b.datetime) return -1;
+    return 0;
+  });
+
   $.get('/../template/sidebar_template.html', function(data){
     theTemplate = Handlebars.compile(data);
   }).done(function(){
