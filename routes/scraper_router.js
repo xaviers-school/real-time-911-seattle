8b4b679580e3a4d392ff39aa4bb8fbe66d787779
Natalie Chow <cychownat@gmail.com>
2016-05-25 14:08:03 -0700

    Refactored scraper and added ability to scrape multiple days at once

--- routes/scraper_router.js
+++ routes/scraper_router.js
@@ -1,67 +1,28 @@
 const express = require('express');
-const request = require('request');
-const cheerio = require('cheerio');
+const moment = require('moment');
 
-const handleDBError = require(__dirname + '/../lib/handle_db_error');
-const combineDuplicates = require(__dirname + '/../lib/combine_duplicates');
-const updateDB = require(__dirname + '/../lib/update_db');
+const scrapeOne = require(__dirname + '/../lib/scrape_one');
+const scrapeMultiple = require(__dirname + '/../lib/scrape_multiple');
 
 var scraperRouter = module.exports = exports = express.Router();
 
-// specific url for today data
-const url = 'http://www2.seattle.gov/fire/realtime911/getRecsForDatePub.asp?action=Today&incDate=&rad1=des';
-// url for special date:
-// const url = 'http://www2.seattle.gov/fire/realtime911/getRecsForDatePub.asp?incDate=5-20-2016&rad1=des'
-const fields = ['displayDatetime', 'incidentNumber', 'level', 'units', 'location', 'type'];
-
-// here using request
 scraperRouter.get('/', function(req, res) {
-  request(url, function(error, response, html) {
-    if (error)
-      return res.status(500).json({ msg: 'Error reaching Seattle Real Time 911' });
-
-    // here cheerio loads data into html
-    var $ = cheerio.load(html);
-    var $data = $("#row_1").parent();
-
-    var allData = [];
-    $data.children().each(function(index) {
-      var incident = {};
-      var $row = $(this);
-
-      // extract each cell and put data into object
-      $row.children()
-        .each(function(i, cell) {
-          switch (fields[i]) {
-            case 'level':
-              incident[fields[i]] = Number($(this).text());
-              break;
-            case 'units':
-              incident[fields[i]] = $(this).text().split(' ');
-              break;
-            default:
-              incident[fields[i]] = $(this).text();
-          }
-        });
-
-      // read incident status from class
-      var rowStatus = $row.find('td:first-child').attr('class');
-      incident.status = rowStatus;
-
-      // add datetime to ISO Date object
-      incident.datetime = new Date(incident.displayDatetime);
-
-      // add index for filtering (combining duplicates)
-      incident.index = index;
-
-      allData.push(incident);
+  scrapeOne(res, moment().format('M-D-YYYY'))
+    .then(function(summary) {
+      res.status(200).json({
+        msg: `${summary.combinedData.length} unique incidents scanned for today`
+      });
     });
+});
 
-    console.log('-----> before:', allData.length);
-    var combinedData = combineDuplicates(allData);
-    console.log('-----> after :', combinedData.length);
-
-    updateDB(res, combinedData);
-    res.status(200).json({ msg: `${combinedData.length} unique incidents scanned` })
-  });
+scraperRouter.get('/multiple', function(req, res) {
+  console.log(req.query);
+  scrapeMultiple(res, Number(req.query.period), req.query.endDate)
+    .then(function(data) {
+      var msg = data.reduce(function(prev, curr) {
+        return prev + `${curr.combinedData.length} unique incidents scanned for ${curr.incDate}\n`;
+      }, '');
+      console.log('multiple scrape summary:\n', msg);
+      res.status(200).json({ msg });
+    });
 });
